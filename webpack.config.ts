92f12111801773a76b9a9cfbfd2a034d20ae684a
Abuzar <mamedov@mercurydevelopment.com>
2024-10-01 11:12:01 +0300

    feat: module 1, lesson 2

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,35 +1,20 @@
 import path from 'path';
-import webpack, { type Configuration } from 'webpack';
-import HTMLWebpackPlugin from 'html-webpack-plugin';
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+import type { BuildOptions, BuildPaths } from './config/build/types/config';
 
-const config: Configuration = {
-  mode: 'development',
-  entry: {
-    RANDOM: path.resolve(__dirname, 'src', 'index.ts'),
-  },
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'build'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html'),
-    }),
-    new webpack.ProgressPlugin(),
-  ],
+const paths: BuildPaths = {
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  build: path.resolve(__dirname, 'build'),
+  html: path.resolve(__dirname, 'public', 'index.html'),
 };
+
+const mode: BuildOptions['mode'] = 'development';
+const isDev = mode === 'development';
+
+const config = buildWebpackConfig({
+  mode,
+  paths,
+  isDev,
+});
 
 export default config;
