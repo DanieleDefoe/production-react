0d33a000351db5326ce799d30c4c55dcf4c2eefc
Abuzar Mamedov <abuzar.mamedov@yandex.ru>
2024-10-03 21:25:02 +0300

    feat: module 2, lesson 1

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,18 +1,6 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  Routes,
-} from 'react-router-dom';
-import App from '../App';
-import { MainPage } from '@/pages/MainPage';
-import { AboutPage } from '@/pages/AboutPage';
+import { createBrowserRouter } from 'react-router-dom';
+import { routeConfig } from './lib/config/routeConfig';
 
-const routes = createRoutesFromElements(
-  <Route path="/" element={<App />}>
-    <Route index element={<MainPage />} />
-    <Route path="about" element={<AboutPage />} />
-  </Route>
-);
+const routes = Object.values(routeConfig);
 
 export const router = createBrowserRouter(routes);
---
+++ src/app/router/lib/config/routeConfig.tsx
@@ -0,0 +1,31 @@
+import App from '@/app/App';
+import { AboutPage } from '@/pages/AboutPage';
+import { MainPage } from '@/pages/MainPage';
+import { RouteObject } from 'react-router-dom';
+
+export enum AppRoutes {
+  MAIN = 'main',
+  ABOUT = 'about',
+}
+
+export const RoutePath: Record<AppRoutes, string> = {
+  [AppRoutes.MAIN]: '/',
+  [AppRoutes.ABOUT]: '/about',
+};
+
+export const routeConfig: RouteObject[] = [
+  {
+    path: RoutePath[AppRoutes.MAIN],
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <MainPage />,
+      },
+      {
+        path: RoutePath[AppRoutes.ABOUT],
+        element: <AboutPage />,
+      },
+    ],
+  },
+];
