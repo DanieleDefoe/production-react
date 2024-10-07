be46e67cbcd981bca47fecead710cd07ce05e910
Abuzar <abuzar.mamedov@i01.ai>
2024-10-07 23:28:45 +0300

    feat: update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -67,6 +67,7 @@ export default tsEslint.config(
       '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
+      'max-len': ['error', { ignoreComments: true }],
     },
   }
 );
